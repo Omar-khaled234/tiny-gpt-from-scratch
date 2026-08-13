2a1dc96be6b94a26fc58835bf2c38caff5ae327d
Devi234 <ok1234master@gmail.com>
2026-08-14 02:04:13 +0300

    Step 18: elementwise_multiply

--- model.py
+++ model.py
@@ -146,8 +146,13 @@ def elementwise_add(a, b):
     b = np.array(b)
     return a+b
 
-# Step 18 - elementwise_multiply (not yet solved)
-# TODO: implement
+# Step 18 - elementwise_multiply
+import numpy as np
+
+def elementwise_multiply(a, b):
+    """Return the elementwise product of two same-shape arrays."""
+    # TODO: compute the elementwise (Hadamard) product of a and b
+    return a*b
 
 # Step 19 - scalar_broadcast_add (not yet solved)
 # TODO: implement
